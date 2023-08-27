d4e194927369dd0d63d9a2bee8a946b3b5e8d8bc
RobertDickie182 <robertdanieldickie@gmail.com>
2023-08-27 15:06:36 +0100

    dagger test passing

--- src/test/java/WeaponsTest/DaggerTest.java
+++ src/test/java/WeaponsTest/DaggerTest.java
@@ -1,4 +1,27 @@
 package WeaponsTest;
 
+import Characters.Bad.Dragon;
+import Weapons.Dagger;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DaggerTest {
+
+    Dagger dagger;
+    Dragon dragon;
+
+    @Before
+    public void before(){
+        dragon = new Dragon(80);
+        dagger = new Dagger("Pointy");
+    }
+
+    @Test
+    public void daggerCanAttack(){
+        dagger.attack(dragon);
+        dragon.getEnemyHealth();
+        assertEquals(75, dragon.getEnemyHealth());
+    }
 }
