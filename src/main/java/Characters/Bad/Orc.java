82d2db198547e5c06e82ac5cec4580dc255f2017
RobertDickie182 <robertdanieldickie@gmail.com>
2023-10-11 19:59:53 +0100

    orc tests passing

--- src/main/java/Characters/Bad/Orc.java
+++ src/main/java/Characters/Bad/Orc.java
@@ -1,7 +1,14 @@
 package Characters.Bad;
 
+import Characters.Good.Dwarf;
+import Characters.Good.Player;
+
 public class Orc extends Enemy {
     public Orc(int enemyHealth) {
         super(enemyHealth);
+    }
+
+    public void attack(Player player) {
+        player.takeDamage(20);
     }
 }
--- src/test/java/EnemiesTests/OrcTest.java
+++ src/test/java/EnemiesTests/OrcTest.java
@@ -1,4 +1,41 @@
 package EnemiesTests;
 
+import Characters.Bad.Orc;
+import Characters.Good.Dwarf;
+import Weapons.Axe;
+import Weapons.IWeapon;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class OrcTest {
+
+    Orc orc;
+    Dwarf dwarf;
+    IWeapon axe;
+
+    @Before
+    public void before(){
+        orc = new Orc(80);
+        axe = new Axe("Chopper");
+        dwarf = new Dwarf("Coops", 100, axe);
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(80, orc.getEnemyHealth());
+    }
+
+    @Test
+    public void canTakeDamage() {
+        dwarf.attack(orc);
+        assertEquals(55, orc.getEnemyHealth());
+    }
+
+    @Test
+    public void canAttack() {
+        orc.attack(dwarf);
+        assertEquals(80, dwarf.getPlayerHealth());
+    }
 }
