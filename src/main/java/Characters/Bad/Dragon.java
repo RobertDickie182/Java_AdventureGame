ce45a50e5aa3f4ecbeb87da76a4369dc2e392658
RobertDickie182 <robertdanieldickie@gmail.com>
2023-10-11 20:07:41 +0100

    dragon tests passing

--- src/main/java/Characters/Bad/Dragon.java
+++ src/main/java/Characters/Bad/Dragon.java
@@ -1,7 +1,14 @@
 package Characters.Bad;
 
+import Characters.Good.Knight;
+import Characters.Good.Player;
+
 public class Dragon extends Enemy {
     public Dragon(int enemyHealth) {
         super(enemyHealth);
+    }
+
+    public void attack (Player player) {
+        player.takeDamage(50);
     }
 }
--- src/test/java/EnemiesTests/DragonTest.java
+++ src/test/java/EnemiesTests/DragonTest.java
@@ -1,4 +1,40 @@
 package EnemiesTests;
 
+import Characters.Bad.Dragon;
+import Characters.Good.Knight;
+import Weapons.Sword;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DragonTest {
+
+    Dragon dragon;
+    Knight knight;
+    Sword sword;
+
+    @Before
+    public void before(){
+        dragon = new Dragon(100);
+        sword  = new Sword("Omega");
+        knight = new Knight("Danny", 100, sword);
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(100, dragon.getEnemyHealth());
+    }
+
+    @Test
+    public void canTakeDamage() {
+        knight.attack(dragon);
+        assertEquals(85, dragon.getEnemyHealth());
+    }
+
+    @Test
+    public void canAttack() {
+        dragon.attack(knight);
+        assertEquals(50, knight.getPlayerHealth());
+    }
 }
