798a9df678d6d593ed360859d7d59219e64fa7c5
RobertDickie182 <robertdanieldickie@gmail.com>
2023-10-12 10:39:14 +0100

    added healing items tests

---
+++ src/test/java/HealingTests/PotionTest.java
@@ -0,0 +1,4 @@
+package HealingTests;
+
+public class PotionTest {
+}
--- src/test/java/HeroTests/ClericTest.java
+++ src/test/java/HeroTests/ClericTest.java
@@ -1,4 +1,30 @@
 package HeroTests;
 
+import Characters.Good.Cleric;
+import Characters.Good.Knight;
+import Healing.Herbs;
+import Healing.IHeal;
+import Weapons.IWeapon;
+import Weapons.Sword;
+import org.junit.Before;
+
 public class ClericTest {
+
+    IHeal herbs;
+
+    Cleric cleric;
+
+    IWeapon sword;
+
+    Knight knight;
+
+
+    @Before
+    public void before(){
+        herbs = new Herbs("Green", 40);
+        cleric = new Cleric("Marlene", 100, herbs);
+        sword = new Sword("Excalibur")
+        knight = new Knight("Calum", 50, sword );
+    }
+
 }
